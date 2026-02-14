a3c75c03efb76d771a6668cee4a3cfc64bf12c97
Михаил Гусев <mihail.gusev2009@gmail.com>
2026-02-14 22:12:03 +0300

    Алелуя база заработала

--- app/views.py
+++ app/views.py
@@ -3,9 +3,50 @@ from django.http.request import HttpRequest
 from .models import Student
 from django.utils import timezone
 import json
+from datetime import date
 # Create your views here.
 
 def test_view(request):
+    Student.objects.create(student_id=1,
+        ball_pro=80,
+        ball_rus=0,      # добавьте все поля
+        ball_mat=0,
+        ball_ind=0,
+        ball_sum=0,
+        priority1=1,
+        priority2=0,     # все priority должны быть заполнены
+        priority3=0,
+        priority4=0,
+        agreement1=True,
+        agreement2=False,
+        agreement3=True,
+        agreement4=True,
+        date=date(2025, 8, 1))
+    '''try:
+        
+        newStudent = Student(
+        student_id=1,
+        ball_pro=80,
+        ball_rus=0,      # добавьте все поля
+        ball_mat=0,
+        ball_ind=0,
+        ball_sum=0,
+        priority1=1,
+        priority2=0,     # все priority должны быть заполнены
+        priority3=0,
+        priority4=0,
+        agreement1=True,
+        agreement2=False,
+        agreement3=True,
+        agreement4=True,
+        date=date(2025, 8, 1)
+    )
+        newStudent.save()
+        print("Запись успешно создана!")
+        print(newStudent)
+    except Exception as e:
+        print(f"Ошибка: {e}")'''
+
     return render(request, "index.html")
 
 def all_students_view(request: HttpRequest):
@@ -17,16 +58,17 @@ def all_students_view(request: HttpRequest):
     return render(request, "all_students.html", context=context)
 
 def rep_all_view(request: HttpRequest):
+    students = Student.objects.all()
     context=dict()
     report_date = timezone.now()
-    data = {
+    data: dict[dict[str | int | any]] = {
         'pm': {
-            'total_applications': 156,
+            'total_applications': 0,#156,
             'places': 40,
-            'priority_1_apps': 45,
-            'priority_2_apps': 40,
-            'priority_3_apps': 35,
-            'priority_4_apps': 36,
+            'priority_1_apps': 0,#45,
+            'priority_2_apps': 0,#40,
+            'priority_3_apps': 0,#35,
+            'priority_4_apps': 0,#36,
             'enrolled_1': 25,
             'enrolled_2': 10,
             'enrolled_3': 5,
@@ -73,7 +115,16 @@ def rep_all_view(request: HttpRequest):
             'last_day_score': 188,
         }
     }
-    
+    for student in students:
+        if student.priority1 == 1:
+            data['pm']['priority_1_apps']+=1
+        elif student.priority1 == 2:
+            data['pm']['priority_2_apps']+=1
+        elif student.priority1 == 3:
+            data['pm']['priority_3_apps']+=1
+        elif student.priority1 == 4:
+            data['pm']['priority_4_apps']+=1
+    data['pm']['total_applications'] = data['pm']['priority_1_apps']+data['pm']['priority_2_apps']+data['pm']['priority_3_apps']+data['pm']['priority_4_apps']
     # Данные для графиков (динамика за 4 дня)
     dynamics_dates = ['11.02', '12.02', '13.02', '14.02']
     dynamics = {
