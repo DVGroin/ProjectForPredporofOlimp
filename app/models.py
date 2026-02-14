29fed020c2101eb948b0493f97b880bd7b16d9be
Михаил Гусев <mihail.gusev2009@gmail.com>
2026-02-14 12:11:31 +0300

    Переписал запись о поступающих,

--- app/models.py
+++ app/models.py
@@ -3,12 +3,33 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
+    """
+    ID : айди человека
+    agreement1 : соглашения на поступление
+    agreement2
+    agreement3
+    agreement4
+    ball_pro : баллы за проф, рус, математику, индивидульный проект, сумма балов
+    ball_rus 
+    ball_mat 
+    ball_ind 
+    ball_sum 
+    priority1 : приоритеты на поступления (соответсвуют с соглашениями)
+    priority2
+    priority3 
+    priority4 
+    """
     ID = models.IntegerField()
-    agreement = models.BooleanField()
-    priority = models.IntegerField()
+    agreement1 = models.BooleanField()
+    agreement2 = models.BooleanField()
+    agreement3 = models.BooleanField()
+    agreement4 = models.BooleanField()
     ball_pro = models.IntegerField()
     ball_rus = models.IntegerField()
     ball_mat = models.IntegerField()
     ball_ind = models.IntegerField()
     ball_sum = models.IntegerField()
-    napr = models.IntegerField()
+    priority1 = models.IntegerField()
+    priority2 = models.IntegerField()
+    priority3 = models.IntegerField()
+    priority4 = models.IntegerField()
