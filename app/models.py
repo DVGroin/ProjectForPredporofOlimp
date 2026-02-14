59d68c86c572ed4eb2e223b1d0b4c2e31645c0a9
Михаил Гусев <mihail.gusev2009@gmail.com>
2026-02-14 11:51:23 +0300

    Добавил модель для базы данных

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    ID = models.IntegerField()
+    agreement = models.BooleanField()
+    priority = models.IntegerField()
+    ball_pro = models.IntegerField()
+    ball_rus = models.IntegerField()
+    ball_mat = models.IntegerField()
+    ball_ind = models.IntegerField()
+    ball_sum = models.IntegerField()
+    napr = models.IntegerField()
